2d83c260693b14a045493cf97a014f2b632d9427
Brinda <brindatreesabiju10@gmail.com>
2023-02-11 18:03:43 +0530

    first code change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ContactpageComponent } from './pages/contactpage/contactpage.component';
+import { DistrictpageComponent } from './pages/districtpage/districtpage.component';
+import { LoginpageComponent } from './pages/loginpage/loginpage.component';
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { FooterComponent } from './utils/footer/footer.component';
+import { SingleComponent } from './single/single.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ContactpageComponent,
+    DistrictpageComponent,
+    LoginpageComponent,
+    NavbarComponent,
+    FooterComponent,
+    SingleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
